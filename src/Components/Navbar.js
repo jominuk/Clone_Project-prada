e0306249ec84e8aea5c34d6c4ce4ca82a429c43f
jominuk <jominuk1025@naver.com>
2022-12-28 11:05:48 +0900

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,6 +13,7 @@ import {
   setAuthenticate,
 } from "../Redux/modules/listSlice";
 import CatergoryNavbar from "./CatergoryNavbar";
+import InputMode from "./InputMode";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -29,6 +30,12 @@ const Navbar = () => {
     // authenticate ? setAuthenticate(false) : navigate("/login");
     authenticate ? dispatch(setAuthenticate(false)) : setIsOpen(true);
   };
+
+  const onSignUpHandler = () => {
+    navigate("/signup");
+    setIsOpen(false);
+  };
+
   const search = (e) => {
     if (e.key === "Enter") {
       navigate(`/?q=${e.target.value}`);
@@ -75,16 +82,26 @@ const Navbar = () => {
               <ModalBackdrop>
                 <StModal>
                   <ModalLogin>
-                    <h1>login</h1>
+                    <StLoginTitle>login</StLoginTitle>
                     <div>로그인을 하시면 빠른 결제가 가능합니다.</div>
                     <StInput placeholder="이메일 *" />
                     <StInput type="password" placeholder="비밀 번호 *" />
+                    <StContentLogin>
+                      <StContents>
+                        <InputMode />
+
+                        <StContent>Remember me</StContent>
+                      </StContents>
+                      <StLoginButton>로그인</StLoginButton>
+                    </StContentLogin>
                   </ModalLogin>
 
                   <MdadalView>
                     <StSocialLogin>소셜 로그인 </StSocialLogin>
                     <StEnrollment>등록하지 않으셨나요?</StEnrollment>
-                    <StSignup>새 계정 만들기</StSignup>
+                    <StSignup onClick={() => onSignUpHandler()}>
+                      새 계정 만들기
+                    </StSignup>
                   </MdadalView>
 
                   <StModalClose onClick={() => setIsOpen(false)}>
@@ -245,7 +262,7 @@ const StModal = styled.div`
   justify-content: space-between;
   width: 100%;
   height: 690px;
-  background-color: black;
+  background-color: #051f1dff;
   left: 0;
   top: -15px;
   animation: modal 0.8s;
@@ -286,7 +303,7 @@ const StSocialLogin = styled.div`
 `;
 
 const StInput = styled.input`
-  background-color: black;
+  background-color: #051f1dff;
   border: none;
   border-bottom: 2px solid #ccc;
   width: 130%;
@@ -309,9 +326,9 @@ const StSignup = styled.button`
   z-index: 0;
   color: white;
   position: relative;
-  margin: 20px 0 0 0;
+  margin: 45px 0 0 0;
   font-weight: bold;
-  font-size: 23px;
+  font-size: 20px;
   background-color: transparent;
   border: none;
   border-bottom: 1px solid white;
@@ -338,6 +355,38 @@ const StSignup = styled.button`
   :hover {
     color: black;
   }
+`;
+
+const StLoginTitle = styled.div`
+  font-size: 35px;
+  font-weight: bold;
+  margin: 10px 0 15px 0;
+`;
+
+const StContents = styled.div`
+  display: flex;
+`;
+
+const StContent = styled.div`
+  margin: 70px 0 0 20px;
+  font-size: 20px;
+`;
+
+const StContentLogin = styled.div`
+  width: 131%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StLoginButton = styled.button`
+  margin: 65px 0 0 0;
+  width: 110px;
+  height: 50px;
+  color: black;
+  background-color: gray;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
 `;
 
 export default Navbar;
--- src/Redux/modules/loginSlice.js
+++ src/Redux/modules/loginSlice.js
@@ -1,69 +1,52 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { instance } from "../../instance/instance";
-import setToken from "../../Pattern/setToken";
-import { setCookie } from "../../shared/cookie";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { instance } from "../../instance/instance";
+// import setToken from "../../Pattern/setToken";
+// import { setCookie } from "../../shared/cookie";
 
-export const __loginUser = createAsyncThunk(
-  "LOGIN_USER",
-  async (payload, thunkAPI) => {
-    try {
-      setToken();
-      const { data } = await instance.post("", payload);
-      setCookie("token", data.token, {
-        //받아온 토큰을 쿠키에 저장하는 역할을
-        //setCookie가 한다.
-        //브라우저에서 쿠키가 있으니, API요청이 필요할 떄 토큰을 줘야한다.
-        //토큰을 서버에 보내준다.
-        //토큰에 담아서 회원정보를 보내준다.
-        //토큰은 프론트에도 가지고 있어야 누군지 알기 때문에
-        //ex 위시리스트에 등록할때 누구인지 구별을 해야하기 때문에
-        //받아온 순간 set쿠키를 써서 토큰을 저장 한다.
-        //로그인할 떄 get쿠키는 토큰에
-        //로그인을 전역 상태로 관리
-        //토큰 유효성 검사도 해야한다
-        //로그인 상태 유지
-        path: "/",
-        expire: "after60m",
-      });
-      localStorage.setItem("", data); //로그인할 때 받은 쿠키를 꺼낸다?
-      alert("로그인 성공!");
-      return thunkAPI.fulfillWithValue(payload);
-    } catch ({ response }) {
-      alert("아이디어와 비밀번호를 다시 확인해주세요.");
-      return thunkAPI.rejectWithValue(response);
-    }
-  }
-);
+// export const __loginUser = createAsyncThunk(
+//   "LOGIN_USER",
+//   async (payload, thunkAPI) => {
+//     try {
+//       setToken();
+//       const data = await instance.post("", payload);
+//       setCookie("token", data.token, {
+//         path: "/",
+//         expire: "after60m",
+//       });
+//       // localStorage.setItem("", data);
+//       alert("로그인 성공!");
+//       return thunkAPI.fulfillWithValue(data);
+//     } catch ({ response }) {
+//       alert("아이디어와 비밀번호를 다시 확인해주세요.");
+//       return thunkAPI.rejectWithValue(response);
+//     }
+//   }
+// );
 
-const initialState = {
-  email: "",
-  password: "",
-  isLoading: false,
-  error: "",
-};
+// const initialState = {
+//   email: "",
+//   password: "",
+//   isLoading: false,
+//   error: "",
+// };
 
-const userSlice = createSlice({
-  name: "LOGIN_USER",
-  initialState,
-  reducers: {
-    loginCheck: (state, action) => {
-      state.login = action.payload;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(__loginUser.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__loginUser.fulfilled, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__loginUser.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.data.message;
-      });
-  },
-});
+// const userSlice = createSlice({
+//   name: "LOGIN_USER",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(__loginUser.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(__loginUser.fulfilled, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(__loginUser.rejected, (state, action) => {
+//         state.isLoading = false;
+//         state.error = action.payload.data.message;
+//       });
+//   },
+// });
 
-export const { loginCheck } = userSlice.actions;
-export default userSlice.reducer;
+// export default userSlice.reducer;
